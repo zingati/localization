efeb150a269a4740000adb7ab9c05050dd2e9a08
kevin Cishahayo <56118340+zingati@users.noreply.github.com>
2024-03-14 18:51:44 +0800

    Add files via upload
    getting Ip localization

---
+++ IP_address_someone's.py
@@ -0,0 +1,12 @@
+import socket
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+print("Your Computer Name is:" + hostname)
+print("Your Computer IP Address is:" + IPAddr)
+#How to get the IP address of a client using socket
+
+
+
+
+
+
